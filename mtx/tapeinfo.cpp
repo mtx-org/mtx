0ecbd41fa926a52b985b7aa6e777199b1e290b9e
William Sutton <will@sutton-family.org>
2020-11-05 09:57:40 -0500

    Update tapeinfo.cpp
    Changed improper number of argument errors into something more readable. If run in the background and errors are expected to be captured, these errors should be easier to spot/correct.

--- mtx/tapeinfo.cpp
+++ mtx/tapeinfo.cpp
@@ -932,7 +932,15 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		fprintf(stderr,"argc=%d",argc);
+                if (argc < 3)
+                {
+                        fprintf(stderr,"ERROR: Too few arguments\n");
+                }
+                if (argc > 3)
+                {
+                        fprintf(stderr,"ERROR: %d too many arguments\n", argc-3);
+                }
+
 		usage();
 	}
 
